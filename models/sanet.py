ae47563fa5fc27f439b9041c3c7ca79197d518ee
kaigelee <88812684+kaigelee@users.noreply.github.com>
2023-07-13 17:20:54 +0800

    Add files via upload

---
+++ models/sanet.py
@@ -0,0 +1,70 @@
+# ------------------------------------------------------------------------------
+# Written by Jiacong Xu (jiacong.xu@tamu.edu)
+# ------------------------------------------------------------------------------
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import time
+import logging
+
+BatchNorm2d = nn.BatchNorm2d
+bn_mom = 0.1
+algc = False
+
+## TODO
+
+
+def get_pred_model(name,num_classes):
+
+
+    return None
+
+if __name__ == '__main__':
+    
+    # Comment batchnorms here and in model_utils before testing speed since the batchnorm could be integrated into conv operation
+    # (do not comment all, just the batchnorm following its corresponding conv layer)
+    device = torch.device('cuda')
+    model = get_pred_model(name='SANet', num_classes=19)
+    model.eval()
+    model.to(device)
+    iterations = None
+    
+    input = torch.randn(1, 3, 1024, 2048).cuda()
+    with torch.no_grad():
+        for _ in range(10):
+            model(input)
+    
+        if iterations is None:
+            elapsed_time = 0
+            iterations = 100
+            while elapsed_time < 1:
+                torch.cuda.synchronize()
+                torch.cuda.synchronize()
+                t_start = time.time()
+                for _ in range(iterations):
+                    model(input)
+                torch.cuda.synchronize()
+                torch.cuda.synchronize()
+                elapsed_time = time.time() - t_start
+                iterations *= 2
+            FPS = iterations / elapsed_time
+            iterations = int(FPS * 6)
+    
+        print('=========Speed Testing=========')
+        torch.cuda.synchronize()
+        torch.cuda.synchronize()
+        t_start = time.time()
+        for _ in range(iterations):
+            model(input)
+        torch.cuda.synchronize()
+        torch.cuda.synchronize()
+        elapsed_time = time.time() - t_start
+        latency = elapsed_time / iterations * 1000
+    torch.cuda.empty_cache()
+    FPS = 1000 / latency
+    print(FPS)
+    
+    
+    
+
+
